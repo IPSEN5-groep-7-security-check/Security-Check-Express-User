6f58ad59c9e2178f30c80c0d2f1c0cdcdbfcc6ab
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-05-20 14:07:19 +0200

    Merge app.js and server.js
    Fixed PDF generation and ports

--- server.js
+++ server.js
@@ -7,10 +7,77 @@
 const PORT = 8080;
 const MOZILLA_API_URL = "https://http-observatory.security.mozilla.org/api/v1/";
 
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const index = require("./routes/index");
+const users = require("./routes/users");
+const pdf = require("./routes/pdf");
+const email = require("./routes/email");
+const cors = require("cors");
+
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const express = require("express");
 const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "public/images"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", index);
+app.use("/users", users);
+app.use("/pdf", pdf);
+app.use("/sendemail", email);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  //  res.render("error");
+});
 
 // INVOKE ASSESSMENT
 // See: https://github.com/mozilla/http-observatory/blob/master/httpobs/docs/api.md#invoke-assessment
@@ -33,10 +100,10 @@ app.post("/api/v1/analyze", async (req, res) => {
   const host = req.query.host;
   const rescan = req.query.rescan || false;
   const moz = await fetch(
-    `${MOZILLA_API_URL}/analyze?host=${host}&hidden=true&rescan=${rescan}`,
-    {
-      method: "POST",
-    }
+      `${MOZILLA_API_URL}/analyze?host=${host}&hidden=true&rescan=${rescan}`,
+      {
+        method: "POST",
+      }
   );
   const json = await moz.json();
   res.json(json);
