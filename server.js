e203bc6d6c1dbf16a351329faf53c736a0df646b
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-06-21 20:52:15 +0200

    clean up last

--- server.js
+++ server.js
@@ -72,23 +72,6 @@ app.use("/users", users);
 app.use("/pdf", pdf);
 app.use("/sendemail", email);
 
-// I commented this out because it didn't work
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
 async function isHostnameBanned(host) {
   const bannedHostname = await prisma.hostnameBlacklist.findUnique({
     where: {
