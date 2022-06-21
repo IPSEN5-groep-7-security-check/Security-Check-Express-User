9519d5f3dafa4e3522112d786a8629bf4d5b0982
Hayyan55 <62708524+Hayyan55@users.noreply.github.com>
2022-06-21 20:29:41 +0200

    Deccryption and refactoring (#10)
    * install node-forge
    
    co-authored-by: Frank van den Broek <s1130543@student.hsleiden.nl>
    
    * WIP decryption functionaliteit
    
    co-authored-by: Frank van den Broek <s1130543@student.hsleiden.nl>
    
    * Update gitignore
    
    co-authored-by: Frank van den Broek <s1130543@student.hsleiden.nl>
    
    * decryption gemaakt
    
    * Clean-up + private key fix
    
    Co-authored-by: Frank van den Broek <s1130543@student.hsleiden.nl>
    Co-authored-by: Loek <loek@appel-snijder.nl>

--- routes/email.js
+++ routes/email.js
@@ -1,9 +1,10 @@
+const forge = require('node-forge')
 const express = require('express')
 const router = express.Router()
 const nodemailer = require('nodemailer');
 const axios = require("axios");
 const fs = require("fs");
-const crypto = require("crypto");
+require("crypto");
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -13,80 +14,61 @@ const transporter = nodemailer.createTransport({
         pass: 'onsMooiWachtwoord'
     }
 });
+const PRIVATE_KEY = fs.readFileSync("privateKey.key.pem", "utf8");
 
-function fillPdfFileWithRightInfo(encryptedData) {
-    const mailOptions = {
-        from: 'getbigmarketingresultaat@gmail.com',
-        to: encryptedData.email,
-        subject: "Uw scan resultaten",
-        text: "Beste " + encryptedData.name + ",\n\n In de PDF vindt u de resultaten van de security check.\n\n Met vriendelijke groet, \n\n Get Big Marketing",
-        attachments: [
-            {
-                filename: 'Test-Resultaten\: ' + "'" + encryptedData.host + "'" + '.pdf',
-                path: 'pdf/resultaten_' + path + '.pdf'
-            }]
-    };
-    return mailOptions;
-}
-
-function sendmail(mailOptions, res){
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error)
-            res.send(error)
-        } else {
-            res.send({"text" : 'Email sent: ' + info.response})
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
-
-function emailPdfGenerator(encryptedData) {
+function emailPdfGenerator(decryptedData, path) {
     if (fs.existsSync('pdf/resultaten_' + path + '.pdf')) {
-        sendmail(fillPdfFileWithRightInfo(encryptedData), res);
-    } else {
-        axios.post("http://localhost:8080/sendemail", req.body).then(() => {
-        })
+        const mailOptions = {
+            from: 'getbigmarketingresultaat@gmail.com',
+            to: decryptedData.email,
+            subject: "Uw scan resultaten",
+            text: "Beste " + decryptedData.name + ",\n\n In de PDF vindt u de resultaten van de security check.\n\n Met vriendelijke groet, \n\n Get Big Marketing",
+            attachments: [
+                {
+                    filename: 'Test-Resultaten\: ' + "'" + decryptedData.host + "'" + '.pdf',
+                    path: 'pdf/resultaten_' + path + '.pdf'
+                }]
+        };
+        sendmail(mailOptions, decryptedData)
     }
 }
 
 router.post('/', async (req, res) => {
     let path = null;
-    const encryptedData = req.body;
-    console.log("RECEIVED ENCRYPTED BODY: " + encryptedData);
-
-    const privateKey = fs.readFileSync("privateKey.key.pem", "utf8");
-    console.log("toDecryptData::: " + encryptedData);
-
-    const decryptedData = decryptAngularEncryptedDataRequest(encryptedData, privateKey);
-    console.log("DECRYPTED-DATA: " + decryptedData);
-    axios.post("http://localhost:8080/pdf", {host: encryptedData.host}).then(async function (response) {
+    const encryptedUserData = req.body;
+    const decryptedUserData = await decryptUser(encryptedUserData);
+    axios.post("http://localhost:8080/pdf", {host: decryptedUserData.host}).then(async function (response) {
         path = response.data.scan_id;
-
     }).then(() => {
-        emailPdfGenerator(encryptedData);
+        emailPdfGenerator(decryptedUserData, path, res);
     });
 })
 
-function decryptAngularEncryptedDataRequest(encryptedData, privateKey) {
-    // encryptedData = Buffer.from(encryptedData, "base64");
-    const toDecryptData = JSON.parse(encryptedData);
-    // console.log("ENCRYPTED DATA 2222:::: " + encryptedData);
-    console.log("ENCRYPTED DATA 2222:::: " + body);
-    const decryptedData = crypto.privateDecrypt(
-        {
-            key: privateKey,
-            // In order to decrypt the data, we need to specify the
-            // same hashing function and padding scheme that we used to
-            // encrypt the data in the previous step
-            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-            oaepHash: "sha256",
-        },
-        toDecryptData
-    );
-    console.log("decrypted data: ", decryptedData.toString());
-    console.log("DE RAW DATA" + decryptedData);
-    return decryptedData;
+async function decryptUser(encryptedUserData) {
+    const name = await decryptString(encryptedUserData.name);
+    const email = await decryptString(encryptedUserData.email);
+    const host = await decryptString(encryptedUserData.host);
+
+    return {
+        name: name,
+        email: email,
+        host: host,
+    }
+
 }
 
+async function decryptString(encryptedString) {
+    const rsa = forge.pki.privateKeyFromPem(PRIVATE_KEY);
+    return await rsa.decrypt(encryptedString);
+}
+
+function sendmail(mailOptions, res){
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            res.send(error)
+        } else {
+            res.send({"text" : 'Email sent: ' + info.response})
+        }
+    });
+}
 module.exports = router
