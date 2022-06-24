b9c9572679ebcedf6fc9671a42e70d92bb2cc32e
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-06-24 19:58:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -59,8 +59,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
 
 
 
