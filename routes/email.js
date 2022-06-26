aea88ad32cad0e41915ac8b68e88ba9ed9be1186
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-06-26 18:17:54 +0200

    Revert "Variablized API URL for dynamic usage"
    This reverts commit c14f15c8dc4500d1e2df56f0a0ee6489312e7b4c.

--- routes/email.js
+++ routes/email.js
@@ -15,7 +15,6 @@ const transporter = nodemailer.createTransport({
         pass: 'mrdpriigoykjiada'
     }
 });
-const apiURL = "https://security-check-user-express.herokuapp.com";
 const PRIVATE_KEY = fs.readFileSync("privateKey.key.pem", "utf8");
 
 function emailPdfGenerator(decryptedData, path, res) {
@@ -39,7 +38,7 @@ router.post('/', async (req, res) => {
     let path = null;
     const encryptedUserData = req.body;
     const decryptedUserData = await decryptUser(encryptedUserData);
-    axios.post(apiURL + "/pdf", {host: decryptedUserData.host}).then(async function (response) {
+    axios.post("https://security-check-user-express.herokuapp.com/pdf", {host: decryptedUserData.host}).then(async function (response) {
         path = response.data.scan_id;
         await prisma.User.upsert({
             where: {
